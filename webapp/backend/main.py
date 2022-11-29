73c60658427787f5cd5110eae74be4ba00d54cf6
hauntedcupoftea <andydchauhan@gmail.com>
2022-11-29 16:13:07 +0530

    only the websocket remains

--- webapp/backend/main.py
+++ webapp/backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
 from modularCluster import *
 import websocket
 
@@ -11,25 +13,41 @@ def read_root():
 
 @app.get('/allocate/{type_gen}')
 def generate_figure(type_gen: str, city_name:str, n: int, start: str, end: str):
+    # filter the dataframe by time, regardless of what method is chosen.
     df = assign(city_name)
     timedf = timeFilter(df, start, end)
+    # if the type is a heatmap, generate and save a heatmap.html, 
+    # manya will then just boot up that file in a new tab and it'll work.
     if type_gen == "Heatmap":
-        lon = timedf['Longitude'].to_list()
-        lat = timedf['Latitude'].to_list()
-        z = timedf['type'].to_list()
-        return {"type" : "densitymapbox", "lon" : lon, "lat" : lat, "z" : z}
+        hm = genHeatMap(timedf)
+        hm.write_html('webapp/frontend/heatmap.html')
+        return {'status' : 'done'} # manya's code needs to wait for this message
+    
+    # almost the same as heatmap, but with more monkey code required to make it work.
     elif type_gen == "Routes":
-        centers, edges = analyze(timedf, n)
-        longs = []
-        lats = []
-        for i in range(len(centers)):
-            tempcen = centers[i]
-            for j, k in edges[i]:
-                x = tempcen[[j, k], 0]
-                y = tempcen[[j, k], 1]
-                longs.append(x.tolist())
-                lats.append(y.tolist())
-        return {"lats" : lats, "lons" : longs}
+        longs, lats = analyze(timedf, n)
+        fig = go.Figure(go.Scattermapbox(
+            mode = "markers+lines"))
+
+        for i in range(len(lats)):
+            fig.add_trace(go.Scattermapbox(
+                mode = "markers+lines",
+                lon = longs[i],
+                lat = lats[i],
+                marker = {'size': 10, 'color' : 'blue'}))
+
+        fig.update_layout(
+            margin ={'l':15,'t':5,'b':5,'r':15},
+            mapbox = {
+                'center': {'lon': np.average(longs), 'lat': np.average(lats)},
+                'style': "stamen-terrain",
+                'center': {'lon': np.average(longs), 'lat': np.average(lats)},
+                'zoom': 11.2},
+            showlegend=False)
+        # write the html file.
+        fig.write_html("webapp/frontend/graph.html")
+
+        return {'status' : 'done'} # manya's code needs to wait for this message
 
 
 # @app.websocket('/')
