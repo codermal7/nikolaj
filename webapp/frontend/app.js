cb150ccc52ed4b9c8c719fead1ed44a42267cef5
manya706 <sharma.manya721@gmail.com>
2022-11-30 19:40:56 +0530

    added js

---
+++ webapp/frontend/app.js
@@ -0,0 +1,12 @@
+function onclick1(){
+    var x=document.getElementById("police").value;
+    var y=document.getElementById('fromtime').value;
+    var z=document.getElementById('totime').value;
+    alert(x+" "+y+" "+z);
+}
+function onclick2(){
+    var x=document.getElementById("police").value;
+    var y=document.getElementById('fromtime').value;
+    var z=document.getElementById('totime').value;
+    alert(x+" "+y+" "+z);
+}
