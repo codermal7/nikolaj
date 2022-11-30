f72293c8df64a15d4e7a57f09d4baf195ee3deaa
hauntedcupoftea <andydchauhan@gmail.com>
2022-11-30 19:43:46 +0530

    improving file

--- webapp/backend/main.py
+++ webapp/backend/main.py
@@ -3,9 +3,23 @@ import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
 from modularCluster import *
-import websocket
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(debug=True)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000"
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def read_root():
